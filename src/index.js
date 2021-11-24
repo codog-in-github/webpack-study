bf34873bfa0dba320f2694fd2b4456638891e068
zjy <zhaojangyu@gmail.com>
2021-11-24 10:38:37 +0800

    配置更新

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-console.log('process.env.NODE_ENV :', process.env.NODE_ENV)
-if(process.env.NODE_ENV === 'development'){
-    console.log(1)
-}else{
-    console.log(2)
-}
+
+// Babel Input: ES2015 arrow function
+const arrowFunction = [1, 2, 3].map(n => this);
+
+// Babel Output: ES5 equivalent
+const equivalent = [1, 2, 3].map(function(n) {
+  return this;
+});
+
+console.log('arrowFunction :', arrowFunction)
+console.log('equivalent :', equivalent)
+undefined
